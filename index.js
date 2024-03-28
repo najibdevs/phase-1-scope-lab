2a8a475e34a98d7f902fdda2659e59b2d2b82923
najibdevs <najibsoftwareeng@gmail.com>
2024-03-28 09:31:42 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = 'BOB'
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'someone'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else';
+}
